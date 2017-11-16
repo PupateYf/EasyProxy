1c768db27a125ee649f260f2d65f74e5cae3dac3
PupateYf <ice-4.ever@163.com>
2017-11-16 23:16:40 +0800

    feat: init pass through part.

---
+++ config/index.js
@@ -0,0 +1,5 @@
+const env = {
+    port: 3333
+}
+
+module.exports = env
--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+const url = require('url')
 const express = require('express')
-const PORT = 3333
+const config = require('./config')
 
 var app = express()
 
 app.use((req, res, next) => {
     //inspect here
-    console.log(req.host)
+    console.log('url: ', req.url)
+    console.log('port ', url.parse(req.url).port)
+    console.log('protocol: ',req.protocol)
+    console.log('hostname: ',req.hostname)
+    console.log('port: ',req.port)
+    console.log('method: ',req.method)
+    console.log('path: ',req.path)
+    console.log('headers: ',req.headers)
+    // console.log('protocol: ',req.protocol)
+    // console.log('protocol: ',req.protocol)
+    // console.log('protocol: ',req.protocol)
+
 })
 
-var server = app.listen(PORT)
+var server = app.listen(config.port)
