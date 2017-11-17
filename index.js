6020b1dac3330d68eeff6e39bec7a986f9044b0e
PupateYf <ice-4.ever@163.com>
2017-11-17 21:51:42 +0800

    feat: add os info.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const url = require('url')
+const os = require('os')
 const express = require('express')
 const config = require('./config')
 const passThrough = require('./lib/passThrough')
@@ -9,4 +10,14 @@ app.use((req, res, next) => {
     passThrough(req, res, next)
 })
 
-var server = app.listen(config.port)
+var server = app.listen(config.port, () => {
+    console.log('The proxy server run on:')
+    let osInfo = os.networkInterfaces()
+    for(let key in osInfo){
+        osInfo[key].forEach(info => {
+            info.family == 'IPv4'
+            ? console.log(`-- ${info.address}:${config.port}`)
+            : ''
+        })
+    }
+})
