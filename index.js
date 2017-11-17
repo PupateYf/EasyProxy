3f04f8817cb2ce4eceb5042321fb765f874ef5a4
PupateYf <ice-4.ever@163.com>
2017-11-17 21:40:25 +0800

    feat: use request module to pass through.

--- index.js
+++ index.js
@@ -1,23 +1,12 @@
 const url = require('url')
 const express = require('express')
 const config = require('./config')
+const passThrough = require('./lib/passThrough')
 
 var app = express()
 
 app.use((req, res, next) => {
-    //inspect here
-    console.log('url: ', req.url)
-    console.log('port ', url.parse(req.url).port)
-    console.log('protocol: ',req.protocol)
-    console.log('hostname: ',req.hostname)
-    console.log('port: ',req.port)
-    console.log('method: ',req.method)
-    console.log('path: ',req.path)
-    console.log('headers: ',req.headers)
-    // console.log('protocol: ',req.protocol)
-    // console.log('protocol: ',req.protocol)
-    // console.log('protocol: ',req.protocol)
-
+    passThrough(req, res, next)
 })
 
 var server = app.listen(config.port)
